0411572e5e3c143e3691c6551625568af50dc6b4
a0955523155-pixel <a0955523155@gmail.com>
2026-02-11 13:13:35 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HelmetProvider } from 'react-helmet-async'; // 新增這行
+// ★★★ 1. 引入這行
+import { HelmetProvider } from 'react-helmet-async'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider> {/* 包住 App */}
+    {/* ★★★ 2. 用這個包住 <App /> */}
+    <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
